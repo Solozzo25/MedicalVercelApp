19bf54a6576f82555ecaf5da39e77618fa74d16f
Solozzo25 <97914353+Solozzo25@users.noreply.github.com>
2025-03-22 15:23:56 +0100

    Create api-utils.js

---
+++ rc/lib/api-utils.js
@@ -0,0 +1,10 @@
+/**
+ * Funkcja do eksportu wyników do PDF
+ * @param {Object} diagnosisData - Dane diagnozy
+ * @param {Object} treatmentData - Dane rekomendacji leczenia
+ * @param {Object} patientData - Dane pacjenta
+ */
+export function exportToPDF(diagnosisData, treatmentData, patientData) {
+  // W przyszłości implementacja eksportu do PDF
+  alert('Funkcja eksportu do PDF będzie zaimplementowana w przyszłości.');
+}
