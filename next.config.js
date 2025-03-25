de70f67a886f5eac00d53c35ddd98cff5a091686
Solozzo25 <97914353+Solozzo25@users.noreply.github.com>
2025-03-25 22:03:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['randomuser.me'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        pathname: '/api/portraits/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
